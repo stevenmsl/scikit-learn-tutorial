0b08a41da1a9e0a16c2cc5cf1f3cc7ac74343b59
Steven Lin <steven_lx@outlook.com>
2019-06-13 08:37:13 -0700

    Update svm_in_depth.py

--- svm_in_depth.py
+++ svm_in_depth.py
@@ -1,3 +1,4 @@
+#https://jakevdp.github.io/PythonDataScienceHandbook/05.07-support-vector-machines.html
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,3 +9,8 @@ from sklearn.datasets.samples_generator import make_blobs
 X, y = make_blobs(n_samples=50, centers=2, random_state=0, cluster_std=0.60)
 plt.scatter(X[:,0], X[:,1], c=y, s=50, cmap='autumn')
 #%% exploring data  
+print (X.shape)
+print (X)
+#%%
+print (y.shape)
+print (y)
