7296125159994ba92908872695884d7791501adf
Steven Lin <steven_lx@outlook.com>
2019-06-13 08:33:10 -0700

    Add svm_in_depth.py

---
+++ svm_in_depth.py
@@ -0,0 +1,10 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+import seaborn as sns; sns.set()
+#%% generating data
+from sklearn.datasets.samples_generator import make_blobs
+X, y = make_blobs(n_samples=50, centers=2, random_state=0, cluster_std=0.60)
+plt.scatter(X[:,0], X[:,1], c=y, s=50, cmap='autumn')
+#%% exploring data  
