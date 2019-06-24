6badca3223ddd1eab4dc328bcd8175930d4c45eb
Steven Lin <slin@merchantadvancecap.com>
2019-06-24 13:18:51 -0700

    update svm_in_depth.py

--- svm_in_depth.py
+++ svm_in_depth.py
@@ -50,7 +50,20 @@ def plot_svc_decision_function(model, ax=None, plot_support=True):
     x = np.linspace(xlim[0], xlim[1], 30) # 30 samples
     y = np.linspace(ylim[0], ylim[1], 30)
     Y, X = np.meshgrid(y, x)
-
+    xy = np.vstack([X.ravel(), Y.ravel()]).T
+    P = model.decision_function(xy).reshape(X.shape)
+    ax.contour(X, Y, P, colors='k',
+    levels=[-1, 0, 1], alpah=0.5,
+    linestyles=['--', '-', '--'])
+    if plot_support:
+        ax.scatter(model.support_vectors_[:, 0],
+        model.support_vectors_[:, 1],
+        s = 300, linewidth = 1, facecolors='none')
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+#%% 
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn') # X contains the features generated to train the model  
+plot_svc_decision_function(model)
 
 #%% meshgrid
 ax = plt.gca()
