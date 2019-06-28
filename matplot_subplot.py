ce315c59cadd3d17417ef6ac4abe40da5f00f7eb
Steven Lin <slin@mac.local>
2019-06-28 11:32:53 -0700

    Add matplot_subplot.py

---
+++ matplot_subplot.py
@@ -0,0 +1,12 @@
+#https://jakevdp.github.io/PythonDataScienceHandbook/04.08-multiple-subplots.html
+#%% 
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-white')
+import numpy as np
+#%%
+# Axes is the area your plot appears in. 
+# Don’t confuse it with axis. 
+ax1 = plt.axes()
+
+
+#%%
