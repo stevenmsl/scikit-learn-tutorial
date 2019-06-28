bc06c986ee84fcf7f15f43fdd78d7c8a45009a4b
Steven Lin <slin@mac.local>
2019-06-28 15:20:38 -0700

    update matplot_subplot.py

--- matplot_subplot.py
+++ matplot_subplot.py
@@ -7,6 +7,12 @@ import numpy as np
 # Axes is the area your plot appears in. 
 # Don’t confuse it with axis. 
 ax1 = plt.axes()
+# starts at 65% of the width and 65% of the height of the figure
+# the size of the axes is 20% of the width and 20% of the height of the figure
+ax2 = plt.axes([0.65, 0.65, 0.2, 0.2])
+
+#%% vertically stack two axes
+fig = plt.figure()
 
 
 #%%
