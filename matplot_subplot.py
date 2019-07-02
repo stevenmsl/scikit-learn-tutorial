1ee0abc21d63d4cab790d88364b654ba00933810
Steven Lin <steven.mslconsulting@gmail.com>
2019-07-02 13:41:03 -0700

    update matplot_subplot.py

--- matplot_subplot.py
+++ matplot_subplot.py
@@ -14,10 +14,14 @@ ax2 = plt.axes([0.65, 0.65, 0.2, 0.2])
 
 #%% vertically stack two axes
 fig = plt.figure()
-ax1 = fig.add_axes([0.1, 0.5, 0.8, 0.4]) 
-
-
+# The bottom of the upper panel (ax1) 0.5 is the bottom of the lower panel (ax2) 0.1 + 0.4 (height). 
+ax1 = fig.add_axes([0.1, 0.5, 0.8, 0.4],
+xticklabels=[], ylim=(-1.2, 1.2))
+ax2 = fig.add_axes([0.1, 0.1, 0.8, 0.4],
+ylim=(-1.2, 1.2))
+x= np.linspace(0, 10)
+ax1.plot(np.sin(x))
+ax2.plot(np.cos(x))
 #%%
 print (np.linspace(0, 10))
-
 #%%
