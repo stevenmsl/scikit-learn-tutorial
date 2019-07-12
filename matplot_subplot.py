47f41d38f3743aa366938791fd9637343dbe7d9b
Steven Lin <steven.mslconsulting@gmail.com>
2019-07-12 15:57:50 -0700

    update matplot_subplot.py

--- matplot_subplot.py
+++ matplot_subplot.py
@@ -39,4 +39,45 @@ for i in range(1, 7):
     ax = fig.add_subplot(2, 3, i)
     ax.text(0.5, 0.5, str((2, 3, i)),
     fontsize=18, ha='center')
+#%% The whole grid in one go
+fig, ax = plt.subplots(2, 3, 
+sharex='col', # remove inner labels on the grid
+sharey='row'
+)
+for i in range(2):
+    for j in range(3):
+        ax[i, j].text(0.5, 0.5, str((i, j)),fontsize=18, ha='center')
+
+#%% more complicated arrangements
+grid = plt.GridSpec(2, 3, wspace=0.4, hspace=0.3)
+plt.subplot(grid[0, 0])
+plt.subplot(grid[0, 1:])
+plt.subplot(grid[1, :2])
+plt.subplot(grid[1, 2])
+
+#%%
+mean = [0, 0]
+cov = [[1, 1], [1, 2]]
+# Check this link to see why you need to use .T  
+# https://stackoverflow.com/questions/5741372/syntax-in-python-t
+x, y = np.random.multivariate_normal(mean, cov, 3000).T
+fig = plt.figure(figsize=(6, 6))
+grid = plt.GridSpec(4, 4, hspace=0.2, wspace=0.2)
+main_ax = fig.add_subplot(grid[:-1, 1:])
+y_hist = fig.add_subplot(grid[:-1, 0], xticklabels=[], sharey=main_ax)
+x_hist = fig.add_subplot(grid[-1, 1:], yticklabels=[], sharex=main_ax)
+
+#scatter points on the main axes
+main_ax.plot(x, y, 'ok', markersize=3, alpha=0.2)
+# histogram 
+# the histogram on the right bottom
+x_hist.hist(x, 40, histtype='stepfilled',
+orientation='vertical', color='gray')
+x_hist.invert_yaxis()
+
+# the histogram on the left
+y_hist.hist(y, 40, histtype='stepfilled',
+orientation='horizontal', color='gray')
+y_hist.invert_xaxis()
+
 #%%
